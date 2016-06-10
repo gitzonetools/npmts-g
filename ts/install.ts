86bce058b7e078ab52c096644a34b00a79f6b8d9
Phil Kunz <github@philkunz.com>
2016-06-10 17:57:19 +0200

    more fixes

--- ts/install.ts
+++ ts/install.ts
@@ -5,6 +5,7 @@ export let installNpmtsGlobally = function(){
   let requiredNpmtsVersion = "^" + packJson.version;
   if (!plugins.shelljs.which('npmts')) {
     console.log("NPMTS wasn't found globally, so it is being installed now");
+    plugins.shelljs.cd("../..");
     plugins.shelljs.exec("npm install npmts@latest");
   } else {
     let globalNpmtsVersion = plugins.shelljs.exec("npmts -v").stdout;
@@ -12,6 +13,7 @@ export let installNpmtsGlobally = function(){
       console.log("OK! global npmts version satisfies needed version");
     } else {
       console.log("GlobalNPMTS does not satisfy required version, so we are installing it locally.");
+      plugins.shelljs.cd("../..");
       plugins.shelljs.exec("npm install npmts@latest");
     }
   }
